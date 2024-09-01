0428dc059dfe4e974e4619aac2658fe1b53bbd12
Victor Valenzuela <khancerberus@hotmail.cl>
2024-09-01 12:31:34 -0400

    chore: Update POST endpoint in webhook to log request body

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,5 +1,16 @@
 export function POST(request: Request) {
-    return new Response(`Hello, ${request.body}!`)
+    // const discordWebhook = 'https://discord.com/api/webhooks/'
+    // fetch(discordWebhook, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         content: request.body,
+    //     }),
+    // })
+    console.log(JSON.stringify(request.body))
+    return new Response(`Hello, ${JSON.stringify(request.body)}!`)
 }
 
 export function GET(request: Request) {
