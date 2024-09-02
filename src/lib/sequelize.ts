e3e6d472d9d646030cf251dcb07fd31278b91a17
Victor Valenzuela <khancerberus@hotmail.cl>
2024-09-01 23:18:06 -0400

    chore: Update sequelize configuration to use pg dialect

--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -2,4 +2,6 @@ import { Sequelize } from 'sequelize'
 
 const DATABASE_URL = import.meta.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres'
 
-export const sequelize = new Sequelize(DATABASE_URL)
+export const sequelize = new Sequelize(DATABASE_URL, {
+    dialect: require('pg')
+})
